b1e1dc331486c57644a6db63191d89be41cd0fa0
pasandi <pasandiwickramasinghe@gmail.com>
2021-09-24 12:57:53 +0530

    Create and implement Add_Places part

---
+++ app/src/main/java/com/example/wikings/Pasandi/uploadinfo.java
@@ -0,0 +1,41 @@
+package com.example.wikings.Pasandi;
+
+public class uploadinfo {
+    public String imageName;
+    public String imageURL;
+    public String province;
+    public String description;
+    public uploadinfo(){}
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
